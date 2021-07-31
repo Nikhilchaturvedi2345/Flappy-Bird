56cfb02f6d14a77c5a1db9004bb9ce6d3cd1a196
Nikhil <nikhilchaturvedi2345@gmail.com>
2021-07-31 20:10:35 +0530

    Flappy Bird

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-let valuu = 150;
-let score = 0;
+// Declayer All Variables 
+
 let up_pillar = document.getElementById("up_pillar");
 let down_pillar = document.getElementById("down_pillar");
+
+const Option1 = document.getElementById("Option1");
 const startingPage = document.getElementById("starting-Page");
 const Playon = document.getElementById("Play-on");
-let kill;
-
 const bird = document.getElementById("bird");
+
+const cancle = document.getElementById("cancle");
+const Option3 = document.getElementById("Option3");
+
+let valuu = 150;
+let score = 0;
+let kill;
+let up = 1;
+
 document.body.addEventListener("click", function (e) {
     let fbT = parseInt(window.getComputedStyle(bird, null).getPropertyValue("top"));
     let gg = fbT - valuu;
@@ -14,43 +23,42 @@ document.body.addEventListener("click", function (e) {
 
         bird.style.top = `${gg}px`;
     }
-    
+
 });
-let up = 1;
 
 
-    document.addEventListener("touchstart",function () {       
-        let flyfly = setInterval(() => {
-            let fbT = parseInt(window.getComputedStyle(bird, null).getPropertyValue("top"));
-            if (fbT > 200) {
-                
-                bird.style.top = `${fbT - up}px`;
-            } 
-            up += 2.5;
-        }, 100);
-        window.flyfly = flyfly;
-    })
-    document.addEventListener("touchend",function () {
-        clearInterval(flyfly);
-    })
+document.addEventListener("touchstart", function () {
+    let flyfly = setInterval(() => {
+        let fbT = parseInt(window.getComputedStyle(bird, null).getPropertyValue("top"));
+        if (fbT > 200) {
+
+            bird.style.top = `${fbT - up}px`;
+        }
+        up += 2.5;
+    }, 100);
+    window.flyfly = flyfly;
+})
+document.addEventListener("touchend", function () {
+    clearInterval(flyfly);
+})
 
 
 
 window.onload = function run() {
 
     let bird = document.getElementById("bird");
-
     let One = 40;
+
     setInterval(() => {
         if (kill == true) {
-            
+
             let birdTop = parseInt(window.getComputedStyle(bird, null).getPropertyValue("top"));
-            
-            if (birdTop < (screen.height* 0.90)) {
-                bird.style.top = `${birdTop + One}px`;            
+
+            if (birdTop < (screen.height * 0.90)) {
+                bird.style.top = `${birdTop + One}px`;
             }
         }
-        }, 150);
+    }, 150);
 
 }
 
@@ -65,17 +73,17 @@ function SecondPillarValue(x) {
     return 100 - (x + 40);
 }
 
-    
-    // score Function 
-    function scoreFunc() {
-        setInterval(() => {
-            score++;
-            console.log(score);
-            
-        }, 100);
-    }
+
+// score Function 
+function scoreFunc() {
+    setInterval(() => {
+        score++;
+        console.log(score);
+
+    }, 100);
+}
 // scoreFunc();
-    
+
 
 
 
@@ -84,7 +92,7 @@ function SecondPillarValue(x) {
 // Making Logoic 
 
 function GamePlay() {
-PChange = true;
+    PChange = true;
     let Runn = setInterval(() => {
         kill = true;
         // Bird Position 
@@ -124,7 +132,6 @@ PChange = true;
 
 
 
-const Option1 = document.getElementById("Option1");
 Option1.addEventListener("click", function () {
     startingPage.style.display = "none";
     bird.style.display = "block";
@@ -133,12 +140,25 @@ Option1.addEventListener("click", function () {
     Playon.style.display = "block";
     GamePlay();
     window.aa = setInterval(() => {
-            const TopPillarValue = RandomHeight();
-            const BottomPillarValue = SecondPillarValue(TopPillarValue);
-            
-            down_pillar.style.height = `${BottomPillarValue}%`;
-            up_pillar.style.height = `${TopPillarValue}%`;
-            
-        
+        const TopPillarValue = RandomHeight();
+        const BottomPillarValue = SecondPillarValue(TopPillarValue);
+
+        down_pillar.style.height = `${BottomPillarValue}%`;
+        up_pillar.style.height = `${TopPillarValue}%`;
+
+
     }, 3000);
 });
+
+
+
+
+// How to Play pop-up 
+cancle.addEventListener("click",function () {
+    const HTP = document.getElementById("HTP");
+    HTP.style.display = "none";
+})
+
+Option3.addEventListener("click",function () {
+    HTP.style.display = "block";
+})
